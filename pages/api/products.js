974a1fd1977fce0edacc8ab42fa753900d5e0327
Loghin Dan <loghindan494@gmail.com>
2023-11-02 11:09:15 +0200

    update

--- pages/api/products.js
+++ pages/api/products.js
@@ -2,19 +2,28 @@ import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handler(req, res) {
-    const {method} = req;
+    const { method } = req;
     await mongooseConnect();
 
-    if (method === "GET"){
-        res.json(await Product.find())
+    if (method === "GET") {
+        if (req.query?.id) {
+            res.json(await Product.findOne({_id:req.query.id}))
+        } else {
+            res.json(await Product.find())
+        }
     }
 
-    if (method ==="POST"){
-        const {title, description, price} = req.body;
-        const productDoc =await Product.create({
+    if (method === "POST") {
+        const { title, description, price } = req.body;
+        const productDoc = await Product.create({
             title, description, price,
         })
         res.json(productDoc)
     }
-  }
-  
+
+    if(method ==="PUT"){
+        const { title, description, price, _id } = req.body;
+        await Product.updateOne({_id}, {title, description, price})
+        res.json(true)
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
 
