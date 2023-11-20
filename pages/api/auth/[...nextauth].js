726455235d8809cb275a6545c1db17f315da8c5a
Loghin Dan <loghindan494@gmail.com>
2023-11-20 18:05:46 +0200

    dasda

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ import clientPromise from "@/lib/mongodb";
 const adminEmails = ['loghindan494@gmail.com', ''];
 
 export const authOptions = {
-  secret: "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",
+  secret: process.env.GOOGLE_SECRET,
   providers: [
     GoogleProvider({
       clientId: "687999287053-stsmvdk76v891hvp717a36j8d3sd1m64.apps.googleusercontent.com",
