61abdf772bde40a0bbb131f782072c16398752e8
Loghin Dan <loghindan494@gmail.com>
2023-11-20 18:08:27 +0200

    asdasd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['loghindan494@gmail.com', ''];
+const adminEmails = ['loghindan494@gmail.com'];
 
 export const authOptions = {
   secret: process.env.GOOGLE_SECRET,
