7d36f924e95f7fa0c3048d26a8ab09330c4bc8a7
Loghin Dan <loghindan494@gmail.com>
2023-11-23 21:20:10 +0200

    asd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,11 @@
 import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import {MongoDBAdapter} from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
 const adminEmails = ['loghindan494@gmail.com', 'eduardivanov803@gmail.com'];
 
 export const authOptions = {
-  secret: process.env.GOOGLE_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
